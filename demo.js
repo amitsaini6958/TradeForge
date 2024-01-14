8c8494b6fa4ea8f1db45a478ef2e765567d407f7
amitsaini6958 <88495908+amitsaini6958@users.noreply.github.com>
2024-01-14 19:48:13 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,14 @@
+function buy(coin) {
+    alert(`Buying ${coin}`);
+    // Add logic to handle buying
+}
+
+function sell(coin) {
+    alert(`Selling ${coin}`);
+    // Add logic to handle selling
+}
+
+document.getElementById('connectWallet').addEventListener('click', () => {
+    alert('Connecting your wallet...');
+    // Add logic to connect the wallet
+});
